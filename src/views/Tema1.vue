b090fb542dc0ea74c2e27a6386c44db4e90b852e
Va-Gomez <99420453+Va-Gomez@users.noreply.github.com>
2022-03-31 09:22:03 -0500

    Actualizando

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -95,7 +95,7 @@
             .col.mb-4.mb-sm-1
               p.text-small.mb-0  <b>Anexo.</b> Decreto 1166 de 2016, por el cual se modifica el decreto 1069 de 2015, Decreto Único Reglamentario del Sector Justicia, relacionado con la prestación, tratamiento y radicación de las peticiones presentadas verbalmente
             .col-sm-auto
-              a.boton.color-acento-botones.texto-blanco(href="https://www.oas.org/es/sla/ddi/docs/acceso_informacion_base_dc_leyes_pais_CO_12.pdf" target="_blank")
+              a.boton.color-acento-botones.texto-blanco(href="https://dapre.presidencia.gov.co/normativa/normativa/DECRETO%201166%20DEL%2019%20DE%20JULIO%20DE%202016.pdf" target="_blank")
                 span Ver más 
                 i.fas.fa-link
 
@@ -108,7 +108,7 @@
             .col.mb-4.mb-sm-1
               p.text-small.mb-0  <b>Anexo.</b> Ley 1755 de 2015 la cual reglamenta el Derecho Fundamental de Petición.
             .col-sm-auto
-              a.boton.color-acento-botones.texto-blanco(href="https://www.oas.org/es/sla/ddi/docs/acceso_informacion_base_dc_leyes_pais_CO_12.pdf" target="_blank")
+              a.boton.color-acento-botones.texto-blanco(href="https://www.funcionpublica.gov.co/eva/gestornormativo/norma_pdf.php?i=65334" target="_blank")
                 span Ver más 
                 i.fas.fa-link
 
@@ -121,7 +121,7 @@
             .col.mb-4.mb-sm-1
               p.text-small.mb-0  <b>Anexo.</b> Ley 1437 de 2011 por la cual se expide el Código de Procedimiento Administrativo y de lo Contencioso Administrativo
             .col-sm-auto
-              a.boton.color-acento-botones.texto-blanco(href="https://www.oas.org/es/sla/ddi/docs/acceso_informacion_base_dc_leyes_pais_CO_12.pdf" target="_blank")
+              a.boton.color-acento-botones.texto-blanco(href="https://www.funcionpublica.gov.co/eva/gestornormativo/norma_pdf.php?i=41249" target="_blank")
                 span Ver más 
                 i.fas.fa-link
 
@@ -134,13 +134,80 @@
             .col.mb-4.mb-sm-1
               p.text-small.mb-0  <b>Anexo.</b> Decreto 019 de 2012 por el cual se dictan normas para suprimir o reformar regulaciones, procedimientos y trámites innecesarios existentes en la Administración Pública
             .col-sm-auto
-              a.boton.color-acento-botones.texto-blanco(href="https://www.oas.org/es/sla/ddi/docs/acceso_informacion_base_dc_leyes_pais_CO_12.pdf" target="_blank")
+              a.boton.color-acento-botones.texto-blanco(href="https://www.funcionpublica.gov.co/eva/gestornormativo/norma_pdf.php?i=45322" target="_blank")
                 span Ver más 
                 i.fas.fa-link
 
+    p Las entidades tienen tiempos estipulados para dar respuesta a una petición, queja o reclamo en un tiempo de 15 días hábiles después de la recepción. 
+    br
+    h4 Proceso General para la Recepción de PQR:
+    br
+    img(src='@/assets/curso/tema1/vec4.svg', alt='Ubicación de mercancía', class='img-fluid').col-10.pos2
+    br
+    br
+    br
+    figure.mb-5
+      .video
+        iframe(width="942" height="530" src="https://www.youtube.com/embed/PSiBtkl75Hc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    p Hablando de PQR es momento de que veamos las diferencias entre peticiones, quejas y reclamos:
+
+    br
+    TabsB.color-acento-contenido.mb-5
+      .py-4.py-md-5(titulo="Peticiones" :icono="require('@/assets/curso/tema1/1.svg')")
+        .row
+          .col-md-8.mb-4.mb-md-0
+            h4 Transpaletas
+            p Pueden transportar la carga a pequeñas distancias y elevarla algunos centímetros, son útiles para apoyar otros equipos y normalmente se encuentran en los muelles de carga y descarga. Se utilizan en almacenes con dimensiones reducidas y sin mucha altura.
+            p 
+              span.fw-bold a. Manuales: 
+              | se usan en las tareas de agrupación de pedidos y carga y descarga de vehículos.
+            p 
+              span.fw-bold b. Motorizados: 
+              | cuentas con un sistema eléctrico y permiten realizar recorridos más largos que el anterior y se utilizan para el mismo fin.
+
+          .col-md-4
 
 
+      .py-4.py-md-5(titulo="Queja" :icono="require('@/assets/curso/tema1/2.svg')")
+        .row
+          .col-md-8.mb-4.mb-md-0
+            h4 Apiladores
+            p Son equipos capaces de transportar y elevar una paleta, permitiendo no solo desplazar la carga horizontalmente, sino verticalmente y de este modo realizar el trabajo de apilado. Se pueden distinguir los siguientes:
+            p 
+              span.fw-bold a. De elevación manual: 
+              | permiten una elevación limitada, por eso no se deben usar con cargas pesadas, normalmente se utilizan para la carga y descarga de estanterías ubicadas en la recepción y expedición.
+            p 
+              span.fw-bold b. Mixtos: 
+              | tienen tracción manual y eléctrica y es el tipo más usado.
+            p 
+              span.fw-bold c. Autopropulsados: 
+              | realizan el traslado y la elevación de manera motorizada, no se recomiendan para recorridos largos y requieren una superficie lisa para el desplazamiento.
 
+          .col-md-4
+          
+
+      .py-4.py-md-5(titulo="Reclamo" :icono="require('@/assets/curso/tema1/3.svg')")
+        .row
+          .col-md-8.mb-4.mb-md-0
+            h4 Carretillas o montacargas
+            p Son equipos de contrapeso que cumplen con la función de transportar y soportar mediante la parte delantera del vehículo cualquier tipo de carga en pallets o contenedores. Pueden ser de los siguientes tipos:
+            p 
+              span.fw-bold a. Contrabalanceada: 
+              | vehículo autopropulsado que realiza la carga por delante del punto de apoyo mediante horquillas.
+            p 
+              span.fw-bold b. Retráctil: 
+              | es un equipo elevador que puede extender y recoger las horquillas o el mástil sobre si mismo y permite alcanzar alturas de elevación superiores.
+            p 
+              span.fw-bold c. De toma lateral: 
+              | existen trilaterales, las cuales pueden manipular la carga de manera: frontal, izquierda y derecha o también las hay bilaterales (derecha/izquierda). Ofrecen mejor rendimiento cuando son guiadas a través de raíles, permitiendo así una mayor velocidad.
+            p 
+              span.fw-bold d. Recogepedidos: 
+              | se forma por una cabina donde se sitúa el conductor y que se eleva junto con las horquillas para realizar tareas de reposición y 
+              em picking.
+
+          .col-md-4
+
+       
 
 
 </template>

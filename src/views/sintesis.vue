a25084eca3cd3b15f5f0c8757136ea3a553b9004
Va-Gomez <99420453+Va-Gomez@users.noreply.github.com>
2022-04-01 11:57:49 -0500

    Completado

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,6 +2,11 @@
 .curso-main-container.pb-3.animated.fadeIn
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .row
+      p La atención de PQRS en las organizaciones traza el propósito de establecer comunicación directa entre los usuarios y la organización, dentro de la organización se crea el área de servicio al cliente que es la encargada de recibir las PQRS, gestionar las respuestas y entregarlas dentro de los tiempos establecidos, así mismo es el área responsable de validar la satisfacción de los clientes, para potencializar eso momentos de verdad que se presentaron e implementar la acciones correctivas cuando se presentan inconformidades.
+      p El mapa mental que se registra a continuación refuerza los conocimientos en el tema de manejo de peticiones, quejas y reclamos.
+    br
+    br
     .row.justify-content-center
       .col-lg-10.mb-5
         figure

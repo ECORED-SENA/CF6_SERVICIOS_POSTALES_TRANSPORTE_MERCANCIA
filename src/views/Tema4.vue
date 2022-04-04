7237aea0b1894b96abffb9dcad8bfd6dcea57f26
Va-Gomez <99420453+Va-Gomez@users.noreply.github.com>
2022-04-04 17:31:51 -0500

    Actualizado

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -12,7 +12,9 @@
       .row
         p.col-6 <b>Una estrategia es aquella cadena planeada y medida de eventos que permite lograr objetivos predefinidos en el tiempo esperado y con la sustancia de resultados requerida por el objetivo que se persigue.</b>
         p.col-6 Los servicios postales, al igual que todos los procesos de servicio, buscan garantizar su continuidad mediante estrategias dirigidas principalmente a los clientes (no solamente a ellos) que permitan los espacios de desarrollo del negocio, su medida, mejora continua y competitividad en el mercado.  
-        p En la actualidad las empresas pueden disponer de una serie de herramientas tecnológicas que facilitan el proceso estratégico de servicio al cliente generando crecimiento no solo desde lo económico y la participación en el mercado sino también desde el reconocimiento e identificaron de la empresa en el medio, estas herramientas tecnológicas facilitan procesos como:
+        br
+        br
+        p.postm4 En la actualidad las empresas pueden disponer de una serie de herramientas tecnológicas que facilitan el proceso estratégico de servicio al cliente generando crecimiento no solo desde lo económico y la participación en el mercado sino también desde el reconocimiento e identificaron de la empresa en el medio, estas herramientas tecnológicas facilitan procesos como:
       br
       br
       .col-9.pos4
@@ -91,7 +93,7 @@
                   p Las reclamaciones pueden ser realizadas por el remitente, el destinatario o por tercero con autorización. 
     separador
 
-    #t_4_1.titulo-segundo.color-acento-contenido
+    #t_4_2.titulo-segundo.color-acento-contenido
       h2 Ciclo del sistema de PQRS
     .row
       p Todos los procesos conllevan un ciclo y las PQR también, el ciclo de vida de un PQR se inicia con la recepción y radicación de las solicitudes a través de todos los canales, se continúa con el proceso de gestión, mediante la asignación al colaborador o funcionario responsable,  se procede con la gestión y trámite, es decir desarrollar el proceso de atención y seguimiento, posterior se emite la respuesta o resolución y se envía al usuario, esta respuesta puede ser en físico, es decir documento impreso con soporte de recibido o por medio de correo electrónico, posterior se realiza seguimiento a la recepción de la respuesta y se evalúa el grado de satisfacción de la misma, se procede con el cierre, se envía para archivo y se generan los reportes para que hagan parte de los indicadores y alimenten los planes y acciones de mejora. 
